284459a065c55f0379c20daf93027db1aa19e90f
Dwight Nodolf <dnodolf@gmail.com>
2020-04-23 19:30:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,11 +9,7 @@ def sort_array_desc(array) # array of integers
 end
 
 def sort_array_char_count(array)
-    count = 0 
-  while count < array.length
-    array.collect.sort{|word| word.length}
-    count += 1
-  
-  end
+  array.sort{|word| word.length}
+    
   
 end
